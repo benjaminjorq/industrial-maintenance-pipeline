80fb44540ce643fc2a5e29054be2fcb2d46fb817
benjaminjorq <benjaminjorq@gmail.com>
2026-08-26 20:56:50 -0400

    documentar SELECT * en scripts SQL

--- sql/production_orders.sql
+++ sql/production_orders.sql
@@ -1,2 +1,5 @@
+-- Uso de SELECT * justificado por la estructura actual de la fuente, sin impacto relevante en rendimiento.
+-- A medida que los datos escalen, se recomienda seleccionar explícitamente las columnas requeridas.
+
 SELECT * 
 FROM public.production_orders
