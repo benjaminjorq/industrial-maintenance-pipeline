68a6e3aae882442a3464ae24268aad8ecb37e038
benjaminjorq <benjaminjorq@gmail.com>
2026-08-27 14:53:22 -0400

    agregar metadata a la extracción remota SFTP

--- src/extractors/sftp_extractor.py
+++ src/extractors/sftp_extractor.py
@@ -2,6 +2,8 @@ import logging
 from pathlib import Path
 import posixpath
 import paramiko
+import json
+from datetime import datetime, timezone
 
 from src.config.settings import (
     SFTP_HOST, SFTP_PORT, SFTP_USER, 
@@ -11,6 +13,23 @@ from utils.gcs_uploader import upload_file
 from src.config.settings import GCS_BUCKET_BRONZE
 
 logger = logging.getLogger(__name__)
+
+def save_metadata(table_name: str, file_path: Path, generated_at: datetime, remote_filename: str) -> Path:
+    """Genera un archivo JSON con la metadata de la extracción SFTP."""
+    metadata = {
+        "source": "sftp",
+        "table": table_name,
+        "file_name": file_path.name,
+        "file_size_bytes": file_path.stat().st_size,
+        "generated_at": generated_at.isoformat(),
+        "remote_source_file": remote_filename
+    }
+    
+    metadata_path = file_path.parent / "metadata.json"
+    with open(metadata_path, "w", encoding="utf-8") as file:
+        json.dump(metadata, file, indent=4)
+        
+    return metadata_path
 
 def run(table_name: str, bronze_path: Path, ingestion_date: str, remote_filename: str) -> None:
     logger.info("Iniciando extracción SFTP para la tabla externa: '%s'", table_name)
@@ -50,10 +69,18 @@ def run(table_name: str, bronze_path: Path, ingestion_date: str, remote_filename
         
         sftp.get(remote_filepath, str(local_filepath))
         logger.info("Archivo descargado exitosamente en: %s", local_filepath)
-        
-        logger.info("Respaldando archivo SFTP en Cloud Storage (Bronze)")
-        upload_file(local_filepath, GCS_BUCKET_BRONZE, f"{cloud_folder}/{local_filename}")
 
+        now = datetime.now(timezone.utc)
+        metadata_path = save_metadata(table_name, local_filepath, now, remote_filename)
+        
+        logger.info("Respaldando archivo SFTP y metadatos en Cloud Storage (Bronze)")
+
+        upload_csv_ok = upload_file(local_filepath, GCS_BUCKET_BRONZE, f"{cloud_folder}/{local_filename}")
+        upload_meta_ok = upload_file(metadata_path, GCS_BUCKET_BRONZE, f"{cloud_folder}/{metadata_path.name}")
+        
+        if not upload_csv_ok or not upload_meta_ok:
+             raise RuntimeError("Fallo crítico: GCS Uploader retornó False. No se pudo respaldar en la nube.")
+        
     except Exception as error:
         logger.error("Error crítico durante la extracción SFTP de '%s': %s", table_name, error, exc_info=True)
         raise
